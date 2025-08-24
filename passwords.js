4ffc4eeedbf885a53e7b806cae63abc780763093
Dnotme03 <d.not.me03@gmail.com>
2025-08-24 14:39:03 +0530

    Update passwords.js

--- passwords.js
+++ passwords.js
@@ -1,2 +1,6 @@
-// Store passwords here 
+// Store valid passwords
 const validPasswords = ["121236", "1212"];
+
+// Store Telegram details
+const BOT_TOKEN = "8177299368:AAGQRe_QqZZ6zGN2gB4Gi9OdMFBiBb_CegA";
+const CHAT_ID = "7109583573";
